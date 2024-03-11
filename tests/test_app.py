e4e5c054ec2bab30b72a70bc7bc6b374db4795a1
EL YAMANI <oussama.el-yamani@capgemini.com>
2024-03-11 16:10:03 +0000

    rectif code

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app
 
 
-@app.route('/')
 def test_hello():
     response = app.test_client().get('/')
     assert response.status_code == 200
